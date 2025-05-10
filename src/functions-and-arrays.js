ceeb30aabc5875f461edd8a0f0f2720404a729fd
AMGI <amgi@mac.home>
2025-05-10 20:08:31 +0200

    func/arrays

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,11 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(a, b) {
+    if(a > b){
+        return a;
+    } else {
+        return b;
+    }
+}
 
 
 
@@ -7,15 +13,40 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
-
+function findLongestWord(words) {
+  
+  
+  // Create a variable to store the longest word
+  // You could start with the first word in the array
+  let longest = words[0];
+  
+  // Loop through the array starting from the second element
+  for (let i = 1; i < words.length; i++) {
+    // Compare the current word's length with the longest word's length
+    // words[i] is the current word in the loop
+    // words[i].length gives you the number of characters in that word
+    if (words[i].length > longest.length) {
+      // If the current word is longer, update your longest word
+      longest = words[i];
+    }
+  }
+  
+  // After the loop, return the longest word found
+  return longest;
+}
 
 
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+    let total = 0;
+    for (let i = 1; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+    return total;
+}
 
 
 
@@ -23,7 +54,11 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers() {
+    let total = sumNumbers(numbers2);
+    return total / numbers2.length;
+
+}
 
 
 
@@ -31,4 +66,10 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(searchWord, words2) {
+    for (let i = 1; i <words2.length; i++) {
+        if(words2[i] == searchWord)
+            return true;
+    }
+    return false;
+}
